e5ec24c4b029b82ef0f9dd2f15de7a6205e1b5bd
Shaik Jaleel <shaikjaleel270@gmail.com>
2020-08-18 22:46:33 +0530

    Revert "W-SAVE: Add GBWhatsAPP Support (DNM)"
    This reverts commit 28a916b8f8675357f942f18f1b2cd312c8579c4f.

--- app/src/main/java/com/nerdinfusions/statusx/commoners/K.kt
+++ app/src/main/java/com/nerdinfusions/statusx/commoners/K.kt
@@ -10,7 +10,6 @@ object K {
     const val TYPE_VIDEO = 1
 
     var WHATSAPP_STORIES = Environment.getExternalStorageDirectory().absolutePath +"/WhatsApp/Media/.Statuses"
-    var GBWHATSAPP_STORIES = Environment.getExternalStorageDirectory().absolutePath +"/GBWhatsApp/Media/.Statuses"
     var SAVED_STORIES = Environment.getExternalStorageDirectory().absolutePath +"/WhatsApp Stories"
 
 }
